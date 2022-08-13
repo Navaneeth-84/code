e229987beac04b963d891721abad6c3c49e8b216
Navaneeth-84 <navaneeth.dhanush@gmail.com>
2022-08-13 20:00:45 +0530

    beta code commit

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,21 +1,24 @@
 
-tasks=[];
+var tasks = [];
 
-function acceptToDo()
-{
-    var info = document.getElementById('textbox').value;
-    if(info.match(/\S/) && !tasks.includes(info))
-    {
-        tasks.push(info);
-        window.alert(info+" added");
-    }
-    else if(tasks.includes(info))
-    {
-        window.alert("This ToDo already exists!")
-    }
-    else
-    {
-        window.alert("Cannot be blank");
-    }
-    document.getElementById('textbox').value="";
+function acceptToDo() {
+
+  var info = document.getElementById("textbox").value;
+  if (info.match(/\S/) && !tasks.includes(info)) {
+    tasks.push(info);
+    window.alert(info + " added");
+  } else if (tasks.includes(info)) {
+    window.alert("This ToDo already exists!");
+  } else {
+    window.alert("Cannot be blank");
+  }
+  document.getElementById("textbox").value = "";
+
+  const list = document.querySelector(".list");
+  list.innerText="";    
+  tasks.forEach(function (task) {
+    const tempTask = document.createElement("div");
+    tempTask.innerText = task;
+    list.appendChild(tempTask);
+  });
 }
