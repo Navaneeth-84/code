8bb3d34ed84e4fc0baa6e2a7925cba144bb72ada
Navaneeth-84 <navaneeth.dhanush@gmail.com>
2022-08-14 08:43:43 +0530

    pending delete functionality

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,24 +1,28 @@
 
 var tasks = [];
 
+const list = document.querySelector(".list");
+
 function acceptToDo() {
 
   var info = document.getElementById("textbox").value;
   if (info.match(/\S/) && !tasks.includes(info)) {
     tasks.push(info);
-    window.alert(info + " added");
+    // window.alert(info + " added");
+    const tempTask = document.createElement("div");
+    tasks.forEach(function (task) {
+      tempTask.innerText = task;
+      list.append(tempTask);
+    });
+    const btn = document.createElement("button");
+    btn.innerHTML = "delete";
+    list.append(btn);
+
   } else if (tasks.includes(info)) {
     window.alert("This ToDo already exists!");
   } else {
     window.alert("Cannot be blank");
   }
-  document.getElementById("textbox").value = "";
-
-  const list = document.querySelector(".list");
-  list.innerText="";    
-  tasks.forEach(function (task) {
-    const tempTask = document.createElement("div");
-    tempTask.innerText = task;
-    list.appendChild(tempTask);
-  });
+  document.getElementById("textbox").value = ""; 
 }
+
