966aad032d0b34b5a1f61b8d1eb7df4c15e140b0
Navaneeth-84 <navaneeth.dhanush@gmail.com>
2022-08-16 21:19:02 +0530

    final commit

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,28 +1,24 @@
-
 var tasks = [];
 
 const list = document.querySelector(".list");
 
 function acceptToDo() {
-
   var info = document.getElementById("textbox").value;
   if (info.match(/\S/) && !tasks.includes(info)) {
     tasks.push(info);
-    // window.alert(info + " added");
     const tempTask = document.createElement("div");
     tasks.forEach(function (task) {
-      tempTask.innerText = task;
+      tempTask.innerHTML = `<div class='flex-container'>${task}<div><button onclick="deleteTask(task)">delete</button></div></div>`;
       list.append(tempTask);
     });
-    const btn = document.createElement("button");
-    btn.innerHTML = "delete";
-    list.append(btn);
-
   } else if (tasks.includes(info)) {
     window.alert("This ToDo already exists!");
   } else {
     window.alert("Cannot be blank");
   }
-  document.getElementById("textbox").value = ""; 
+  document.getElementById("textbox").value = "";
 }
 
+function deleteTask(task) {
+  list.remove(task);
+}
