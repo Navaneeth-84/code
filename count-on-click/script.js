2b88cc951cce0c5af177ed500e69b2a001044827
Navaneeth <navaneeth.dhanush@gmail.com>
2022-08-25 14:44:10 +0530

    pending UI

--- count-on-click/script.js
+++ count-on-click/script.js
@@ -1,29 +1,8 @@
-// container div
-var scr = document.createElement("div");
-
-// up, counter and down
-var up = document.createElement("p");
-var counter = document.createElement("div");
-var down = document.createElement("p");
-
-up.innerText = ">";
-down.innerText = "<";
-
-scr.className = "container";
-up.className = "clicker up";
-counter.className = "cntr flash";
-counter.innerText = 0;
-down.className = "clicker down";
-
-// append all elements to the body
-document.body.appendChild(scr);
-
 var tmp = document.querySelector(".container");
-tmp.append(up, counter, down);
 
 // increment / decrement functionality
-var incr = document.querySelector(".up");
-var decr = document.querySelector(".down");
+var incr = document.querySelector(".fa-angle-up");
+var decr = document.querySelector(".fa-angle-down");
 
 incr.addEventListener("click", function () {
   num = Number(document.querySelector(".cntr").innerHTML);
