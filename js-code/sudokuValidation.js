c710b9977fe1736bfe2607630af89b7769654665
Navaneeth <navaneeth.dhanush@gmail.com>
2022-08-26 12:12:00 +0530

    corrected code smells

--- js-code/sudokuValidation.js
+++ js-code/sudokuValidation.js
@@ -1,33 +1,31 @@
-arr = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 5, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
-function validate(arr, r, c, val) {
-  var res = false;
-  for (let i = r - 1; i < arr.length; ++i) {
-    // row
-    if (arr[r - 1][i] == val) {
-      res = true;
+function validateSudokuEntry(arr, row, col, entryVal) {
+  /**
+   * validates whether entryVal can be inserted at desired position in the sudoku grid
+   * precondition: arr is an n x n array representing a sudoku grid
+   * post-condition: array 'arr' is modified for an element or undisturbed
+   **/
+  let isValid = false; // flag varaible ( assume initially that the given entry already exists )
+  const userRow = row - 1;
+  const userCol = col - 1;
+  // traverse the specified row looking for entryVal
+  for (let i = userRow; i < arr.length; ++i) {
+    if (arr[userRow][i] == entryVal || arr[userCol]) {
+      isValid = true; // modify flag
     }
   }
+  // traverse the specified column looking for 'entryVal'
   for (let i = 0; i < arr.length; ++i) {
-    // col
-    if (arr[c - 1][i] == val) {
-      res = true;
+    if (arr[userCol][i] == entryVal) {
+      isValid = true; // modify flag
     }
   }
-
-  if (!res) arr[r - 1][c - 1] = val;
-  else console.log(`${val} already exists at arr[${r}][${c}] `);
-  console.log(arr);
+  // print a suitable message to the console
+  if (isValid) {
+    console.log(`${val} already exists at arr[${row}][${col}] `);
+  } else {
+    arr[userRow][userCol] = entryVal;
+    console.log(
+      `${entryVal} is a valid entry and is inserted at arr[${row}][${col}]`
+    );
+  }
 }
-
-validate(arr, 2, 5, 5);
